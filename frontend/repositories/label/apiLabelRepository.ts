1498a4676d3a271a5f121bb055336ae7273307c5
Lívia <a79766@ualg.pt>
2025-06-21 15:59:14 +0100

    .

--- frontend/repositories/label/apiLabelRepository.ts
+++ frontend/repositories/label/apiLabelRepository.ts
@@ -30,7 +30,8 @@ export class APILabelRepository implements LabelRepository {
   async list(projectId: string): Promise<LabelItem[]> {
     const url = `/projects/${projectId}/${this.baseUrl}s`
     const response = await this.request.get(url)
-    return response.data  
+    const labels = response.data.results || response.data
+    return labels.map(toModel)
   }
 
   async findById(projectId: string, labelId: number): Promise<LabelItem> {
